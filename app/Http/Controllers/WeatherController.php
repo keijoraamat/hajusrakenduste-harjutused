c581fc844cc77cd2082987a95a20a709910eed26
Keijo Raamat <raamatkeijo@gmail.com>
2020-04-10 19:37:52 +0300

    added caching data from api

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -7,31 +7,37 @@ use Illuminate\Http\Request;
 class WeatherController extends Controller
 {
   public function index() {
-    //$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
-    //$phi =floatval( filter_input(INPUT_POST, 'latitude', FILTER_SANITIZE_STRING));
-    //$lamda = floatval(filter_input(INPUT_POST, 'longitude', FILTER_SANITIZE_STRING));
     $api_key = getenv('api_key');
     define("CACHE_TIME", 300);
     $fileName='./cache.json';
     $url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID='.$api_key.'&lat=58.2550&lon=22.4919&units=metric';
 
+    if ( file_exists($fileName) && (time() - filemtime($fileName) < CACHE_TIME) ) {
+      $content = file_get_contents($fileName);
+      $isCached="on";
+    } else {
+      $content = file_get_contents($url);
+      $file = fopen($fileName, 'w');
+      fwrite($file, $content);
+      fclose($file);
+      $content = file_get_contents($url);
+      $isCached="ei ole";
+    }
 
-            //if ( file_exists($fileName) && (time() - filemtime($fileName) < CACHE_TIME) ) {
-            //    $content = file_get_contents($fileName);
-            //} else {
-            //    $content = file_get_contents($url);
-            //    $file = fopen($fileName, 'w');
-            //    fwrite($file, $content);
-            //    fclose($file);
-            $content = file_get_contents($url);
-            $json = json_decode($content);
-        //}
-
-
-    //var_dump($json->list[0]->dt_txt);
-    //var_dump($json);
-
-  return view('weather', compact('json'));
+    $json = json_decode($content);
+    $data = array(
+      'name'=>$json->city->name,
+      'icon'=>$json->list[0]->weather[0]->icon,
+      'dt_txt'=>$json->list[0]->dt_txt,
+      'temp'=>$json->list[0]->main->temp,
+      'feels_like'=>$json->list[0]->main->feels_like,
+      'pressure'=>$json->list[0]->main->pressure,
+      'humidity'=>$json->list[0]->main->humidity,
+      'hours'=>date("H",($json->city->sunset-$json->city->sunrise)),
+      'minutes'=>date("i", ($json->city->sunset-$json->city->sunrise)),
+      'isCached'=>$isCached
+      );
+    return view('weather', $data);
 }
 
 }
--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -3,34 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $json->city->name;?> ilm</title>
+    <title>{{$name}} ilm</title>
+    <style>
+        html, body {
+            background-color: #fff;
+            font-weight: 400;
+            height: 100vh;
+            margin: 25;
+        }
+        td:hover {background-color: #f5f5f5;}
+    </style>
 </head>
 <body>
-<h3><?php echo $json->city->name;?> ilm <?php echo $json->list[0]->dt_txt;?></h3> <br>
-<img src="http://openweathermap.org/img/wn/<?php echo $json->list[0]->weather[0]->icon;?>@2x.png">
-    <table>
-        <tr>
-            <th>temperatuur</th>
-            <th> </th>
-        </tr>
-        <tr>
-            <td>mõõdetud: </td>
-            <td>tuntav: </td>
-        </tr>
-        <tr>
-            <td><?php echo $json->list[0]->main->temp;?> &#8451</td>
-            <td><?php echo $json->list[0]->main->feels_like;?>&#8451</td>
-        </tr>
-    </table>
-    Õhurõhk <?php echo $json->list[0]->main->pressure;?> hPa <br>
-    Õhuniiskus <?php echo $json->list[0]->main->humidity;?>%
-    <p>Päeva pikkus: <?php echo date("H", ($json->city->sunrise-$json->city->sunset));?> tundi ja <?php echo date("i", ($json->city->sunrise-$json->city->sunset));?> minutit</p>
-<?php
-    //<form method="POST">
-    //    <input id="phi" name="latitude" placeholder="kraadi ekvaatorist"> <br>
-    //    <input id="lamda" name="longitude" placeholder="kraadi 0 meridiaanist"><br>
-    //    <button name="action" value="change_loc">Hangi ilma andmed</button>
-    //</form>
-?>
+  <h3>{{$name}} ilm {{$dt_txt}}</h3> <br>
+  <img src="http://openweathermap.org/img/wn/{{$icon}}@2x.png"><br>
+  Temperatuur
+  <table class="temperature">
+      <tr>
+          <td>mõõdetud</td>
+          <td>tuntav</td>
+      </tr>
+      <tr>
+          <td>{{$temp}} &#8451</td>
+          <td>{{$feels_like}} &#8451</td>
+      </tr>
+  </table>
+  <br>
+  Õhurõhk {{$pressure}} hPa <br>
+  Õhuniiskus {{$humidity}}%
+  <p>Päeva pikkus: {{$hours}} tundi ja {{$minutes}} minutit</p><br>
+  <p>Andmed {{$isCached}} vahepuhvrist </p>
 </body>
 </html>
